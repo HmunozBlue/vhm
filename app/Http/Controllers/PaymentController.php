3b3b79bdf369f53a4a1decdc4561a1f9b10aeca3
Hamunoz <hmunoz@mibluemedical.com>
2023-10-20 00:02:43 -0600

    FiltroPorFechaPagos

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -18,8 +18,13 @@ class PaymentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        //filtro de fechas desde el form index
+        $dateStart = $request->input('fechaInicio', now()->subDays(5)->format('Y-m-d'));
+        $dateEnd = $request->input('fechaFin', now()->format('Y-m-d'));
+
+        //Consulta a la bd 
         $payments = DB::table('tours')
         ->select('tours.id','tours.guies','travel_allowances.tipeTrip','travel_allowances.travelAllowanceDriver','vehicles.carPlate AS vehicle','boxcars.carPlate AS boxcar',
         'boxcars.tonnage','personas.primerNombre','personas.primerApellido','personas.CUI','tours.created_at')
@@ -27,10 +32,11 @@ class PaymentController extends Controller
         ->join('vehicles', 'vehicles.id', '=', 'tours.vehicleId')
         ->join('boxcars', 'boxcars.id', '=', 'tours.boxCarId')
         ->join('personas', 'personas.id', '=', 'tours.driverId')
+        ->whereBetween('tours.created_at', ["$dateStart 00:01:00","$dateEnd 23:59:59"])
         ->orderBy('tours.created_at', 'DESC')
         ->get();
         //$tour = tour::paginate(5);
-        return view('pagos.index',compact('payments'));
+        return view('pagos.index',compact('payments','dateStart','dateEnd'));
     }
 
     /**
